8d1a54e443aa9a5d4cf208eba5e81d53a989764f
najahhamirahh <najah.talmizi@aemenersol.com>
2026-03-08 14:28:57 +0800

    Add AuthGuard for Access Control to Dashboard and Create Dashboard component with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppPage } from './app.page';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppPage,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [{
   path: '',
@@ -9,6 +11,10 @@ const routes: Routes = [{
 }, {
   path: 'login',
   component: LoginComponent
+},{
+  path: 'dashboard',
+  component: DashboardComponent, 
+  canActivate: [AuthGuard] 
 }];
 
 @NgModule({
