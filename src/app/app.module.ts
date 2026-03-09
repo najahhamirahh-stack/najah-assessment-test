67f31401c128736b95fa0db451751f80672c842c
najahhamirahh-stack <najahamirahtalmizi@gmail.com>
2026-03-09 14:03:30 +0800

    Revert "Edit title for header registration"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRouting } from './app.routing';
 import { AppPage } from './app.page';
-import { LoginComponent } from './login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppPage,
-    LoginComponent,
-    DashboardComponent
+    AppPage
   ],
   imports: [
     BrowserModule,
-    AppRouting,
-    ReactiveFormsModule,
-    HttpClientModule
+    AppRouting
   ],
   bootstrap: [
     AppPage
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { AuthGuard } from './_guards/auth.guard';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [{
   path: '',
   pathMatch: 'full',
-  redirectTo: 'login'
+  redirectTo: 'form'
 }, {
-  path: 'login',
-  component: LoginComponent
-},{
-  path: 'dashboard',
-  component: DashboardComponent, 
-  canActivate: [AuthGuard] 
+  path: 'form',
+  loadChildren: './form/form.module#FormModule'
 }];
 
 @NgModule({
